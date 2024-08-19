24ac083c91c0d24f1c97873e0ce7e468a583f513
DavidVelasquez77 <128778972+DavidVelasquez77@users.noreply.github.com>
2024-08-18 23:54:21 -0600

    quinto commit

--- main.f90
+++ main.f90
@@ -12,146 +12,148 @@ module InventarioModule
 
 end module InventarioModule
 
-    program gestionar_inventario
-        use InventarioModule
-        type (Inventario), dimension(:), allocatable :: inventarios
-        character(len=100) :: filename
-        integer :: num_inventario
-        integer :: opcion
+program gestionar_inventario
+    use InventarioModule
+    type(Inventario), dimension(:), allocatable :: inventarios
+    character(len=100) :: filename
+    integer :: num_inventario
+    integer :: opcion
+
+    do
+        PRINT *, '****************************************'
+        PRINT *, '         SISTEMA DE INVENTARIO          '
+        PRINT *, '****************************************'
+        PRINT *, '1. Cargar Inventario'
+        PRINT *, '2. Cargar Movimientos'
+        PRINT *, '3. Mostrar Inventario'
+        PRINT *, '4. Salir'
+        PRINT *, '****************************************'
+        PRINT *, 'Seleccione una opcion (1-4):'
+        PRINT *, '****************************************'
+        READ (*, *) opcion   
+        
+        select case (opcion)
+            case (1)
+                PRINT *, 'Ingrese el nombre de la ruta del archivo de inventario:'
+                READ *,  filename
+                call analizador(filename)
+            case (2)
+                PRINT *, 'Ingrese el nombre de la ruta del archivo de movimientos:'
+                READ *, filename
+                call analizador(filename)  ! Aquí llamamos al analizador también para los movimientos
+            case (3)
+                call mostrar_inventario()
+            case (4)
+                PRINT *, 'Saliendo del sistema'
+                exit
+            case default
+                PRINT *, 'Opcion no valida'
+        end select
+    end do
+
+contains
+
+    SUBROUTINE parse_line(line)
+        CHARACTER(LEN=*), INTENT(IN) :: line
+        INTEGER :: i 
+        CHARACTER(LEN=200) :: temp_line 
+        INTEGER :: start, end_pos 
+        CHARACTER(LEN=50) :: field(4)
+
+        temp_line = line
+        start = 1
+
+        ! Separar las líneas en campos por ;
+        DO i = 1, 4
+            end_pos = INDEX(temp_line(start:), ';')
+            IF (end_pos == 0 .AND. i == 4) THEN
+                field(i) = temp_line(start:)
+            ELSE    
+                field(i) = temp_line(start:start+end_pos-2)
+                start = start + end_pos
+            END IF
+        END DO
+        
+        print *, field(1)
+        print *, field(2)
+        print *, field(3)
+        print *, field(4)
+    END SUBROUTINE parse_line
+
+    SUBROUTINE parse_line3(line)
+        CHARACTER(LEN=*), INTENT(IN) :: line
+        INTEGER :: i 
+        CHARACTER(LEN=200) :: temp_line 
+        INTEGER :: start, end_pos 
+        CHARACTER(LEN=50) :: field(3)
+
+        temp_line = line
+        start = 1
+
+        ! Separar las líneas en campos por ;
+        DO i = 1, 3
+            end_pos = INDEX(temp_line(start:), ';')
+            IF (end_pos == 0 .AND. i == 3) THEN
+                field(i) = temp_line(start:)
+            ELSE    
+                field(i) = temp_line(start:start+end_pos-2)
+                start = start + end_pos
+            END IF
+        END DO
+        
+        print *, field(1)
+        print *, field(2)
+        print *, field(3)
+    END SUBROUTINE parse_line3
+
+    SUBROUTINE analizador(archivo)
+        character(len=100) :: archivo
+        character(len=100) :: line
+        integer :: ios
+        character(len=50) :: comando
+        character(len=50) :: datos
+        integer :: i, start, end_pos
+
+        open(unit=10, file=archivo, status='old', action='read', iostat=ios)
+        if (ios /= 0) then
+            PRINT *, 'Error al abrir el archivo'
+            stop
+        end if
 
         do
-            PRINT *, '****************************************'
-            PRINT *, '         SISTEMA DE INVENTARIO          '
-            PRINT *, '****************************************'
-            PRINT *, '1. Cargar Inventario'
-            PRINT *, '2. Cargar Movimientos'
-            PRINT *, '3. Mostrar Inventario'
-            PRINT *, '4. Salir'
-            PRINT *, '****************************************'
-            PRINT *, 'Seleccione una opcion (1-4):'
-            PRINT *, '****************************************'
-            READ (*, *) opcion   
-            
-            select case (opcion)
-                case (1)
-                    PRINT *, 'Ingrese el nombre de la ruta del archivo de inventario:'
-                    READ *,  filename
-                    call analizador(filename)
-                case (2)
-                    PRINT *, 'Ingrese el nombre de la ruta del archivo de movimientos:'
-                    READ *, filename
-                case (3)
-                    PRINT *, 'Ingrese el nombre de la ruta:'
-                    READ *, filename
-                case (4)
-                    PRINT *, 'Saliendo del sistema'
-                    exit
-                case default
-                    PRINT *, 'Opcion no valida'
+            read(10, '(A)', iostat=ios) line
+            if (ios /= 0) EXIT
+            start = 1
+            end_pos = SCAN(line(start:), ' ')
+            IF (end_pos == 0) THEN
+                comando = TRIM(line(start:))
+                datos = ''
+            ELSE 
+                comando = TRIM(line(start:start+end_pos-2))
+                datos = TRIM(line(start+end_pos:))
+            END IF
+
+            select case (comando)
+            case ('crear_equipo')
+                print *, 'Crear equipo'
+                call parse_line(datos)
+            case ('agregar_stock')
+                print *, 'Agregar stock'
+                call parse_line3(datos)
+            case ('eliminar_stock')
+                print *, 'Eliminar stock'
+                print *, datos
+            case default
+                print *, 'Comando no valido'
             end select
+
         end do
+        close(10)
+    end SUBROUTINE analizador
 
-        contains
-        SUBROUTINE parse_line(line)
-            CHARACTER(LEN=*), INTENT(IN) :: line
-            INTEGER :: i 
-            CHARACTER(LEN=200) :: temp_line 
-            INTEGER :: start, end_pos 
-            CHARACTER(LEN=50) :: field(4)
-    
-            temp_line = line
-            start = 1
-    
-            !separar las lineas en campos por ;
-            DO i = 1,4
-                end_pos = INDEX(temp_line(start:), ';')
-                IF (end_pos == 0 .AND. i == 4) THEN
-                    field(i) = temp_line(start:)
-                ELSE    
-                    field(i) = temp_line(start:start+end_pos-2)
-                    start = start + end_pos
-                END IF
-            END DO
-            
-            print *, field(1)
-            print *, field(2)
-            print *, field(3)
-            print *, field(4)
-        
-            
-        END SUBROUTINE parse_line
+    SUBROUTINE mostrar_inventario()
+        ! Implementación para mostrar el inventario
+        PRINT *, 'Función mostrar inventario aún no implementada.'
+    END SUBROUTINE mostrar_inventario
 
-        SUBROUTINE parse_line3(line)
-            CHARACTER(LEN=*), INTENT(IN) :: line
-            INTEGER :: i 
-            CHARACTER(LEN=200) :: temp_line 
-            INTEGER :: start, end_pos 
-            CHARACTER(LEN=50) :: field(4)
-    
-            temp_line = line
-            start = 1
-    
-            !separar las lineas en campos por ;
-            DO i = 1,3
-                end_pos = INDEX(temp_line(start:), ';')
-                IF (end_pos == 0 .AND. i == 3) THEN
-                    field(i) = temp_line(start:)
-                ELSE    
-                    field(i) = temp_line(start:start+end_pos-2)
-                    start = start + end_pos
-                END IF
-            END DO
-            
-            print *, field(1)
-            print *, field(2)
-            print *, field(3)
-        
-            
-        END SUBROUTINE parse_line3
-
-
-            SUBROUTINE analizador(archivo)
-                character(len=100) :: archivo ! nombre del archivo
-                character(len=100) :: line !variable para leer linea por linea
-                integer :: ios !error del archivo
-                character(len=50) :: comando !comando a ejecutar agregar stocker, eleminar stock
-                character(len=50) :: datos !obtener la linea
-                integer :: i, start, end_pos ! variables para leer y separ datos
-
-                open(unit=10, file=archivo, status='old', action='read', iostat=ios)
-                if (ios /= 0) then
-                    PRINT *, 'Error al abrir el archivo'
-                    stop
-                end if
-
-                do
-                    read(10, '(A)', iostat=ios) line
-                    if (ios /= 0) EXIT
-                    start = 1
-                    end_pos = SCAN(line(start:), ' ')
-                    IF (end_pos == 0) THEN
-                        comando = TRIM(line(start:))
-                        datos = ''
-                    ELSE 
-                        comando = TRIM(line(start:start+end_pos-2))
-                        datos = TRIM(line(start+end_pos:))
-                    END IF
-
-                    select case (comando)
-                    case ('crear_equipo')
-                        print *, 'Crear equipo'
-                        call parse_line(datos)
-                    case ('agregar_stock')
-                        print *, 'Agregar stock'
-                        call parse_line3(datos)
-                    case ('eliminar_stock')
-                        print *, 'Eliminar stock'
-                        print *, datos
-                    case default
-                        print *, 'Comando no valido'
-                    end select
-
-                end do
-            end SUBROUTINE analizador
-
-    end program gestionar_inventario
+end program gestionar_inventario
